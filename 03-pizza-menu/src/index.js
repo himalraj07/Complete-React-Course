d7d2b33f7346d27cc258aaac1ba1181195bd3259
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-06-30 21:37:30 +0545

    conditional rendering with &&

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -69,15 +69,21 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -123,7 +129,14 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} - We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>
+            We're open until {closeHour}:00. Come visit us or under outline.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 
