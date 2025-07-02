ab117c537bca7f9d9440fde6bef78ca13e5ab314
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-07-02 08:30:36 +0545

    conditional rendering with ternaries

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -71,18 +71,20 @@ function Header() {
 function Menu() {
   const pizzas = pizzaData;
   // const pizzas = [];
-  const numPizzas = pizzas.length
+  const numPizzas = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
 
       {/* <Pizza
@@ -121,7 +123,7 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
-  const isOpen = hour >= openHour && hour < closeHour;
+  const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
   // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
@@ -129,13 +131,17 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>
             We're open until {closeHour}:00. Come visit us or under outline.
           </p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
