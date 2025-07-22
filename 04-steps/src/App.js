05bad3ea6dc69271c538d25593cd2410595fd1df
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-07-22 18:35:35 +0545

    more reusability with the 'children' prop

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -10,6 +10,14 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>😎</p>
+      </StepMessage>
       {/* <Steps /> */}
     </div>
   );
@@ -50,10 +58,18 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}:{messages[step - 1]}
-            {/* {test.name} */}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
@@ -70,7 +86,16 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, children}) {
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
